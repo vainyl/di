127461d907d1cc8bb3eb682872262daabf7940e3
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-05-10 14:06:15 +0300

    Added AbstractContainerFactoryDecorator

---
+++ src/Decorator/AbstractContainerFactoryDecorator.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Vainyl
+ *
+ * PHP Version 7
+ *
+ * @package   Di
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @link      https://vainyl.com
+ */
+declare(strict_types=1);
+
+namespace Vainyl\Di\Decorator;
+
+use Psr\Container\ContainerInterface;
+use Vainyl\Core\Application\EnvironmentInterface;
+use Vainyl\Di\Factory\ContainerFactoryInterface;
+
+/**
+ * Class AbstractContainerFactoryDecorator
+ *
+ * @author Taras P. Girnyk <taras.p.gyrnik@gmail.com>
+ */
+abstract class AbstractContainerFactoryDecorator implements ContainerFactoryInterface
+{
+    private $containerFactory;
+
+    /**
+     * AbstractContainerFactoryDecorator constructor.
+     *
+     * @param ContainerFactoryInterface $containerFactory
+     */
+    public function __construct(ContainerFactoryInterface $containerFactory)
+    {
+        $this->containerFactory = $containerFactory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function createContainer(EnvironmentInterface $descriptor): ContainerInterface
+    {
+        return $this->containerFactory->createContainer($descriptor);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getId(): string
+    {
+        return $this->containerFactory->getId();
+    }
+}
