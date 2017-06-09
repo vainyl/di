3e043d34a4149eb389f6534fb225d36dd8656e82
Taras Girnyk <taras.p.gyrnik@gmail.com>
2017-06-09 18:06:02 +0300

    Removed unused code

--- src/Factory/SymfonyContainerFactory.php
+++ src/Factory/SymfonyContainerFactory.php
@@ -14,12 +14,9 @@ namespace Vainyl\Di\Factory;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\ContainerInterface as SymfonyContainerInterface;
-use Symfony\Component\DependencyInjection\Dumper\PhpDumper;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Vainyl\Core\AbstractIdentifiable;
 use Vainyl\Core\Application\EnvironmentInterface;
-use Vainyl\Di\Exception\UnableToCacheContainerException;
 
 /**
  * Class SymfonyContainerFactory
@@ -38,57 +35,6 @@ class SymfonyContainerFactory extends AbstractIdentifiable implements ContainerF
     public function __construct(ContainerBuilder $containerBuilder)
     {
         $this->containerBuilder = $containerBuilder;
-    }
-
-    /**
-     * @param EnvironmentInterface $environment
-     *
-     * @return ContainerBuilder
-     */
-    public function initContainer(EnvironmentInterface $environment): ContainerBuilder
-    {
-        $this->containerBuilder->set('app.environment', $environment);
-        $loader = new YamlFileLoader($this->containerBuilder, new FileLocator($environment->getConfigDirectory()));
-        $loader->load($environment->getContainerConfig());
-
-        foreach ($environment->getExtensions() as $extension) {
-            $extension->load([], $this->containerBuilder, $environment);
-        }
-
-        $this->containerBuilder->compile();
-
-        return $this->containerBuilder;
-    }
-
-    /**
-     * @param EnvironmentInterface $environment
-     *
-     * @return SymfonyContainerInterface
-     */
-    public function getCachedContainer(EnvironmentInterface $environment): SymfonyContainerInterface
-    {
-        $containerPath = sprintf(
-            '%s/container/%s.php',
-            $environment->getCacheDirectory(),
-            sha1($environment->__toString())
-        );
-
-        if (file_exists($containerPath)) {
-            require_once $containerPath;
-
-            return new \CompiledContainer();
-        }
-
-        $container = $this->initContainer($environment);
-        if (false === file_exists(dirname($containerPath))) {
-            mkdir(dirname($containerPath), 0755, true);
-        }
-        $dumper = new PhpDumper($container);
-        if (false === file_put_contents($containerPath, $dumper->dump(['class' => 'CompiledContainer']))) {
-            throw new UnableToCacheContainerException($this, $container, $environment, $containerPath);
-        }
-
-        return $container;
     }
 
     /**
